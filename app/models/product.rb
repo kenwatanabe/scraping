8b225bb048e4da6fa8988a163e25824241a71054
Ubuntu <ubuntu@rails-dev-box>
2017-12-09 12:08:36 +0000

    記事と商品と中間テーブルのアソシエーションを作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ActiveRecord::Base
     has_many :genres, through: :genre_products
     has_many :genre_products
     has_many :benefit
+    has_many :article_products
     has_one  :amazon_api_datum
 end
